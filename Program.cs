8b77007360583f13346d4fa5ef0a6c367578a8f3
Andrew-Creswick-Student <ancr131@student.oaisd.org>
2024-10-07 16:49:05 +0000

    Created a Program that greets the user and tells them about me.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Andrew Creswick");
+
+        //Variables!!!
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Andrew";
+        string lastname = "Creswick";
+        double money = 50;
+        
+        Console.WriteLine(greeting+"! "+ "My name is "+ firstname + " "+ lastname+". I am "+age+ " years old. I just spent $"+money+" on a lego.");
+    }
+}
